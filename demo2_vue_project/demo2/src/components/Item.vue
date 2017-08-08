5b76f7820ffb2888654528e6a6baaf916c73e3e2
zhhgit <zhh900601@sina.com>
2017-08-08 17:24:15 +0800

    demo2

---
+++ demo2_vue_project/demo2/src/components/Item.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-if = "see">
+      哈哈哈
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'item',
+    data: function () {
+      return {
+        see: true
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
