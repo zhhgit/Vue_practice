a438a49cbf4ec209a3854dc8c3cd73b10054fcb5
zhhgit <zhh900601@sina.com>
2017-08-08 20:03:44 +0800

    demo2 v-on:click

---
+++ demo2_vue_project/demo2/src/components/Page5.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button v-on:click="say('zhanghao')">调用methods中的say('zhanghao')</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'page5',
+    data: function () {
+      return {
+        message: 'hello'
+      }
+    },
+    methods: {
+      say: function (name) {
+        alert(this.message + ' ' + name)
+      }
+    }
+  }
+</script>
--- demo2_vue_project/demo2/src/router/index.js
+++ demo2_vue_project/demo2/src/router/index.js
@@ -1,39 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import Page1 from '@/components/Page1'
 import Page2 from '@/components/Page2'
 import Page3 from '@/components/Page3'
 import Page4 from '@/components/Page4'
+import Page5 from '@/components/Page5'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/hello',
-      name: 'Hello',
-      component: Hello
-    },
+    // v-if
     {
       path: '/page1',
       name: 'Page1',
       component: Page1
     },
+    // v-for
     {
       path: '/page2',
       name: 'Page2',
       component: Page2
     },
+    // computed and methods
     {
       path: '/page3',
       name: 'Page3',
       component: Page3
     },
+    // v-bind:class
     {
       path: '/page4',
       name: 'Page4',
       component: Page4
+    },
+    // v-on:click
+    {
+      path: '/page5',
+      name: 'Page5',
+      component: Page5
     }
   ]
 })
