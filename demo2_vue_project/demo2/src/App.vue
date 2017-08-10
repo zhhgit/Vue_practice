6284a48e300b96b6079c3039c5493cd62287bab9
zhhgit <zhh900601@sina.com>
2017-08-10 09:54:29 +0800

    demo2 add router-Link

--- demo2_vue_project/demo2/src/App.vue
+++ demo2_vue_project/demo2/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <LinkArea></LinkArea>
+    <hr />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import LinkArea from './components/LinkArea.vue'
+  export default {
+    name: 'app',
+    components: {
+      LinkArea
+    }
+  }
 </script>
---
+++ demo2_vue_project/demo2/src/components/LinkArea.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <router-link to="/page1">Page1 v-if</router-link><br />
+    <router-link to="/page2">Page2 v-for</router-link><br />
+    <router-link to="/page3">Page3 computed and methods</router-link><br />
+    <router-link to="/page4">Page4 v-bind:class</router-link><br />
+    <router-link to="/page5">Page5 v-on:click</router-link><br />
+    <router-link to="/page6">Page6 v-html</router-link><br />
+    <router-link to="/page7">Page7 v-models</router-link><br />
+    <router-link to="/page8">Page8 vue组件,动态props,自定义事件,自定义指令</router-link>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'linkArea'
+  }
+</script>
